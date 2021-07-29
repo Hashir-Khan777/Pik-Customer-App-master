2b8ecc01c7ae52765f34bcae010086a3ba38a1a7
Hashir Khan <khnhashir555@gmail.com>
2021-07-29 13:34:29 +0500

    set language

--- src/utils/i18n/index.js
+++ src/utils/i18n/index.js
@@ -1,43 +1,40 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import AsyncStorage from '@react-native-community/async-storage'
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import EN from './langs/en'
-import ES from './langs/es'
+import EN from './langs/en';
+import ES from './langs/es';
 
 i18n
-    .use(initReactI18next) // passes i18n down to react-i18next
-    .init({
-        resources: {
-            en: {translation: EN},
-            es: {translation: ES},
-        },
-        fallbackLng: "es",
-        lng: "es",
-        keySeparator: '.',
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources: {
+      en: {translation: EN},
+      es: {translation: ES},
+    },
+    fallbackLng: 'es',
+    lng: 'es',
+    keySeparator: '.',
 
-        interpolation: {
-            escapeValue: false // react already safes from xss
-        }
-    });
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  });
 
 async function loadStoredLanguage() {
-    const lang = await AsyncStorage.getItem('current_lang');
-    if(!!lang && lang != i18n.language){
-        await i18n.changeLanguage(lang)
-    } else {
-        await i18n.changeLanguage('en');
-    }
+  const lang = await AsyncStorage.getItem('current_lang');
+  if (!!lang && lang != i18n.language) {
+    await i18n.changeLanguage('en');
+  } else {
+    await i18n.changeLanguage('en');
+  }
 }
 
-async function storeCurrentLanguage(lang){
-    await AsyncStorage.setItem('current_lang', lang);
+async function storeCurrentLanguage(lang) {
+  await AsyncStorage.setItem('current_lang', lang);
 }
 
-// loadStoredLanguage()
+loadStoredLanguage();
 
 export default i18n;
-export {
-    i18n,
-    storeCurrentLanguage,
-}
+export {i18n, storeCurrentLanguage};
